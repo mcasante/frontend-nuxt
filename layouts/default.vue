1be9d875fba984aa96df555e841e19fd0ac6ba3e
Matheus Casante <matheus.casante@gmail.com>
2024-08-18 00:42:32 -0300

    feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="flex justify-center shadow-md" style="box-shadow: 0 0 24px 0 rgb(230, 230, 230)">
+    <u-container class="py-6 flex gap-4 justify-end">
+      <u-link
+        to="/frontend"
+        active-class="text-primary"
+        inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+        >frontend</u-link
+      >
+      <u-link
+        to="/backend"
+        active-class="text-primary"
+        inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+        >backend</u-link
+      >
+      <u-link
+        to="/about-me"
+        active-class="text-primary"
+        inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+        >about me</u-link
+      >
+    </u-container>
+  </header>
+  <main>
+    <u-container class="py-8">
+      <slot />
+    </u-container>
+  </main>
+</template>
