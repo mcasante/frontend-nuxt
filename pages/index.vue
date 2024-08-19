e138bd84629edf7b8103742ddc3ccef8d82d84b3
Matheus Casante <matheus.casante@gmail.com>
2024-08-18 21:36:48 -0300

    refactor: use seller card

--- pages/index.vue
+++ pages/index.vue
@@ -5,16 +5,8 @@ const sellers = await $fetch<Seller.ISeller[]>("/api/sellers");
 </script>
 
 <template>
-  <h1 class="text-4xl">Orders</h1>
-
-  <div class="flex gap-4 justify-end mt-8">
-    <u-card v-for="seller in sellers">
-      <template #header>
-        <span>Total of {{ seller.name }}</span>
-      </template>
-
-      <span>{{ seller.totalSales }}</span>
-    </u-card>
+  <div class="flex gap-4 justify-end">
+    <seller-card v-for="seller in sellers" :seller="seller" />
   </div>
 
   <client-only>
