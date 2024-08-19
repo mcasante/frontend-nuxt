1aebacb5be9b1f7fa8e483e973b52bf50915b004
Matheus Casante <matheus.casante@gmail.com>
2024-08-18 21:36:07 -0300

    feat: add index to the header navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,13 @@
 <template>
-  <header class="flex justify-center shadow-md" style="box-shadow: 0 0 24px 0 rgb(230, 230, 230)">
+  <header
+    class="flex justify-center shadow-md"
+    style="box-shadow: 0 0 24px 0 rgb(230, 230, 230)"
+  >
     <u-container class="py-6 flex gap-4 justify-end">
+      <u-link to="/" class="text-2xl text-primary font-bold mr-auto">
+        <h1>Dashboard</h1>
+      </u-link>
+
       <u-link
         to="/frontend"
         active-class="text-primary"
