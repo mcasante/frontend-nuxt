833c95553a2c0f3391fb98401e88c75305cc5062
Matheus Casante <matheus.casante@gmail.com>
2024-08-21 05:37:30 -0300

    feat: add stack list component

---
+++ components/StackList/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{
+  stack: {
+    name: string;
+    url: string;
+    icon: string;
+  }[];
+}>();
+
+const stackItemBtn: any = {
+  padded: false,
+  color: "black",
+  variant: "link",
+  target: "_blank",
+  rel: "noopener noreferrer",
+  size: "xl",
+};
+</script>
+
+<template>
+  <ul class="stack">
+    <li class="mb-1" v-for="tech in props.stack" :key="tech.name">
+      <u-button :icon="tech.icon" :to="tech.url" v-bind="stackItemBtn">
+        {{ tech.name }}
+      </u-button>
+    </li>
+  </ul>
+</template>
